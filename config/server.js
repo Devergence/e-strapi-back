2e451763db1a87796dd570169e40564979458075
githubloverforever <dmitry.prihodko@rhinoda.com>
2020-12-14 23:35:18 +0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd999051d9b4510bfae6334c987503b3f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e74fe9fe-09a6-4968-9c75-1090cc5ed0f9'
+};
